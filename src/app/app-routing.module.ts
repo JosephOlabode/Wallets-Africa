dba948305a6475439122485c7cf594d651d0c272
JosephOlabode <josepholabode17@gmail.com>
2021-04-06 18:44:20 +0000

    implemented the app routing module to enable routing btw components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {UsersComponent} from './users/users.component';
+import {SendMoneyComponent} from './send-money/send-money.component';
+import {RequestMoneyComponent} from './request-money/request-money.component';
+import {AirtimeComponent} from './airtime/airtime.component';
+import {BillsComponent} from './bills/bills.component';
+import {CardComponent} from './card/card.component';
+import {ReportsComponent} from './reports/reports.component';
+import {DocumentationComponent} from './documentation/documentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'send-money',
+    component: SendMoneyComponent
+  },
+  {
+    path: 'request-money',
+    component: RequestMoneyComponent
+  },
+  {
+    path: 'buy-airtime',
+    component: AirtimeComponent
+  },
+  {
+    path: 'pay-bills',
+    component: BillsComponent
+  },
+  {
+    path: 'card',
+    component: CardComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
+  },
+  {
+    path: 'api-documentation',
+    component: DocumentationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
