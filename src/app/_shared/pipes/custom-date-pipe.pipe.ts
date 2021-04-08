528a178987d47bbc4e9a4c46d17e14cecf05ec78
JosephOlabode <josepholabode17@gmail.com>
2021-04-08 11:53:13 +0000

    implemented my custom date pipe

--- src/app/_shared/pipes/custom-date-pipe.pipe.ts
+++ src/app/_shared/pipes/custom-date-pipe.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomDatePipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  /*transform(value: unknown, ...args: unknown[]): unknown {
     return null;
+  }*/
+
+  // tslint:disable-next-line:typedef
+  transform(value: string) {
+    const cleanedString = value.substring(0, value.indexOf(' '));
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(cleanedString, 'dd/MM/yyyy');
   }
 
 }
