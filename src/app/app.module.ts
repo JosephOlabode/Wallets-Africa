7e56a07b01efc701c471141669fb5d42653f388b
JosephOlabode <josepholabode17@gmail.com>
2021-04-08 11:48:34 +0000

    created the date pipe file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { SendMoneyComponent } from './send-money/send-money.component';
 import { RequestMoneyComponent } from './request-money/request-money.component';
 import { AirtimeComponent } from './airtime/airtime.component';
 import { BillsComponent } from './bills/bills.component';
-import { CardComponent } from './card/card.component';
+import {CardComponent} from './card/card.component';
 import { ReportsComponent } from './reports/reports.component';
 import { DocumentationComponent } from './documentation/documentation.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -25,6 +25,8 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatTabsModule} from '@angular/material/tabs';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CustomDatePipePipe } from './_shared/pipes/custom-date-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,8 @@ import {MatInputModule} from '@angular/material/input';
     BillsComponent,
     CardComponent,
     ReportsComponent,
-    DocumentationComponent
+    DocumentationComponent,
+    CustomDatePipePipe
   ],
   imports: [
     BrowserModule,
@@ -54,7 +57,8 @@ import {MatInputModule} from '@angular/material/input';
     MatMenuModule,
     MatTabsModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
