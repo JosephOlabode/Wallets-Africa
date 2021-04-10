30995f609dcd36827df325c916762b5a460b08ec
JosephOlabode <josepholabode17@gmail.com>
2021-04-10 00:43:30 +0000

    used the mat-accordion for the side menu dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Wallets-Africa';
-  menu = false;
-  navbarOpen = true;
-  notify = false;
   constructor() {
   }
-
   // tslint:disable-next-line:typedef
-  toggleMenu() {
-    this.menu = !this.menu;
-  }
-  // tslint:disable-next-line:typedef
-  closeMenu() {
-    this.menu = false;
-  }
-  // tslint:disable-next-line:typedef
-  logOut() {
-  }
-  // tslint:disable-next-line:typedef
-  toggleNotify() {
-    this.notify = !this.notify;
-  }
-  // tslint:disable-next-line:typedef
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
-  // tslint:disable-next-line:typedef
-  closeNavBar() {
-    this.navbarOpen = false;
-  }
+  logOut() {}
 }
